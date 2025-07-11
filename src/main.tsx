acf20f47a9865bf3c3f9c6ab2717125637eeaba9
Ganesh R <ganesramesh110@gmail.com>
2025-07-11 15:04:42 +0530

    feat: Remove unused dependencies and clean up form component

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ const router = createRouter({
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
+  defaultViewTransition: true,
 });
 
 // Register the router instance for type safety
